0565d1b6e0ee297253c88087c4b343b0cd1bbd68
Kaley Kriminger <kaley.kriminger@gmail.com>
2017-11-07 09:58:35 -1000

    user reducer handles editUser action

--- src/actions/users.js
+++ src/actions/users.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 
 export const LOAD_USERS = 'LOAD_USERS';
 export const ADD_USER = 'ADD_USER';
+export const EDIT_USER = 'EDIT_USER';
 
 export const loadUsers = () => {
   return function(dispatch){
@@ -21,6 +22,18 @@ export const addUser = (newUser) => {
     .then( user => {
       dispatch({
         type: ADD_USER,
+        user: user.data
+      });
+    });
+  }
+}
+
+export const editUser = (newInfo) => {
+  return function(dispatch){
+    return axios.put(`/api/users/${newInfo.id}`, newInfo)
+    .then (user => {
+      dispatch({
+        type: EDIT_USER,
         user: user.data
       });
     });
