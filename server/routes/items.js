1436308e5acf5232f02d21d1c80fd76832d64145
Ba5eem <baseem@gmail.com>
2017-11-06 19:39:09 -1000

    Item get all passes

--- server/routes/items.js
+++ server/routes/items.js
@@ -6,13 +6,13 @@ const LocalStrategy           = require('passport-local').Strategy;
 const saltRounds              = 12;
 const route                   = express.Router();
 const db                      = require('../models');
-const {user}                 = db;
+const {item}                 = db;
 
 
 
 route.get('/', ( req, res ) => {
   console.log('users route has been requested: GET ');
-  user.findAll({raw:true})
+  item.findAll({raw:true})
   .then((DataCollection) => {
     console.log('users route has queried all data from the DB, result: ', DataCollection);
     res.json(DataCollection);
@@ -33,9 +33,14 @@ route.get('/:id', ( req, res ) => {
 route.post('/new', ( req, res ) => {
   console.log('users route has been requested: POST ');
   user.create({
-    username : req.body.username,
-    password : req.body.password,
-    email    : req.body.email
+    name : req.body.name,
+    description : req.body.description,
+    price    : req.body.price,
+    manufacturer : req.body.manufacturer,
+    model : req.body.model,
+    dimensions    : req.body.dimensions,
+    notes : req.body.notes,
+    image    : req.body.image
   }).then((data) => {
     console.log('users route has posted new data to the DB, result: ', data);
     res.json(data);
@@ -48,9 +53,14 @@ route.put('/:id', ( req, res ) => {
   console.log('users.put/:id :', id);
   let data = req.body;
   return user.update({
-    username: req.body.username,
-    password: req.body.password,
-    email: req.body.email
+    name : req.body.name,
+    description : req.body.description,
+    price    : req.body.price,
+    manufacturer : req.body.manufacturer,
+    model : req.body.model,
+    dimensions    : req.body.dimensions,
+    notes : req.body.notes,
+    image    : req.body.image
   }, {where: {id:id}
   }).then((user) => {
     res.json('User updated');
