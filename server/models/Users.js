14a93423fb09377d37799dd865a2f83626171066
Ella Mae A <ellamae207@gmail.com>
2017-11-07 18:05:25 -1000

    initial push for list fix

--- server/models/Users.js
+++ server/models/Users.js
@@ -21,7 +21,7 @@ module.exports = function(sequelize, DataTypes){
 
   User.associate = function(models){
     User.hasMany(models.item, {
-      foreignKey: 'userId'
+      foreignKey: 'userId', as: 'items4sale'
     })
   }
   return User;
