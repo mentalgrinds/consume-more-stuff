0f5c96ad366823d9c7cb02c4cc70f41dcaf657f7
Ba5eem <baseem@gmail.com>
2017-11-08 16:57:43 -1000

    Edit allowed with auth

--- src/components/SingleUser.js
+++ src/components/SingleUser.js
@@ -6,12 +6,15 @@ const SingleUser = ({user,backToUsers,handleSubmit,handleChangeUsername,handleCh
     <div className='eachItem'>
       <ol>
         <button onClick={(e)=>backToUsers(e)}>Back</button>
-        <b>User-Id:</b> {user ? user[0].id : null}<br/>
-        <b>Username:{user ? user[0].username : null}</b><br/>
-        <b>Password:</b> {user ? user[0].password : null}<br/>
-        <b>email:</b> {user ? user[0].email : null}<br/>
-        <b>User-Status</b> {user ? user[0].userstatus : null}<br/>
-        <button onClick={(e)=>edit(user,e)}>EDIT</button>
+          <form onSubmit="this.handleSubmit">
+            <b>User-Id:</b> {user ? user[0].id : null}<br/>
+            <b>Username:{user ? user[0].username : null}</b><br/>
+            <b>Password:</b> {user ? user[0].password : null}<br/>
+            <b>email:</b> {user ? user[0].email : null}<br/>
+            <b>User-Status</b> {user ? user[0].userstatus : null}<br/>
+          </form>
+        {auth ? 
+        <button onClick={(e)=>edit(user,e)}>EDIT</button> : null}
       </ol>
     </div>
   )
