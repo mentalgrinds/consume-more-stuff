d42be0f80b10fe7470610122bd8e0642b518edb6
Ba5eem <baseem@gmail.com>
2017-11-09 10:33:53 -1000

    working on dashboard

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,50 +1,53 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import ItemList from '../../components/ItemList';
-
+import { loadItems,editItem } from '../../actions/items';
+import filterItem from '../../lib/filterUser';
 
 class Dashboard extends Component {
 
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
+
+
+    const userId = localStorage.getItem('userId');
 
     this.state = {
-      authorized: false
+      authorized: false,
+      userItems: filterItem(this.props.items,userId)
     }
-    console.log()
   }
 
-  componentWillMount(){
 
-  }
+  componentWillMount(){ this.props.loadItems();}
 
-  componentDidMount(){
-
-    // this.props.loadItems();
-
-
-    //load items, users, etc.
-  }
 
   render() {
+    const username = localStorage.getItem('username');
+    console.log(this.props.items)
     return (
-      <div className="App">
-{/*        <ItemList />*/}
-
+      <div>
+        <div className="App">
+          <h1>Welcome {username} to your Dashboard View</h1>
+        </div>
+        <div>
+          <ItemList
+            items={this.state.userItems} />
+        </div>
       </div>
     );
   }
 }
 
-const mapStatetoProps = (state) => {
-  return {
-    data : state.items
-
+const mapStateToProps = (state) => {
+  return{
+    items: state.items
   }
 }
 
 const ConnectedDashboard = connect(
-  mapStatetoProps
+  mapStateToProps,
+  {loadItems,editItem}
 )(Dashboard)
 
 export default ConnectedDashboard;
