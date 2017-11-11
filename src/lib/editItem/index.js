73cdb047df6fff10bce04415121020095caf0ba9
Kaley Kriminger <kaley.kriminger@gmail.com>
2017-11-10 19:38:41 -1000

    new helper function to clear local object

--- src/lib/editItem/index.js
+++ src/lib/editItem/index.js
@@ -1,6 +1,6 @@
-let local = {}
+let local = {};
 
-function editHelper(e){
+export function editHelper(e){
 console.log('edit method lib helper has been fired');
   if(e.target.name === 'name')        { local.name=e.target.value; }
   if(e.target.name === 'status')       { local.itemstatusId=parseInt(e.target.value); }
@@ -9,14 +9,18 @@ console.log('edit method lib helper has been fired');
   if(e.target.name === 'price')       { local.price=parseInt(e.target.value); }
   if(e.target.name === 'category')    { local.categoryId=parseInt(e.target.value); }
   if(e.target.name === 'condition')   { local.conditionId=parseInt(e.target.value); }
-  if(e.target.name === 'manfucturer') { local.manfucturer=e.target.value; }
+  if(e.target.name === 'manufacturer') { local.manfucturer=e.target.value; }
   if(e.target.name === 'model')       { local.model=e.target.value; }
   if(e.target.name === 'dimensions')  { local.dimensions=e.target.value; }
   if(e.target.name === 'notes')       { local.notes=e.target.value; }
-  console.log('local', local)
-  return local
 
+  console.log('local', local)
+  return local;
 }
 
-export default editHelper;
+export function clearLocal(){
+  local = {};
+}
+
+export default { editHelper, clearLocal };
 
