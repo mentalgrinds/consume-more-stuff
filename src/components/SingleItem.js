7512062b157a08f422ab45a66be8d288297a3663
Kaley Kriminger <kaley.kriminger@gmail.com>
2017-11-11 11:04:52 -1000

    separates edit form into its own container, adds route action and reducer method for changing image

--- src/components/SingleItem.js
+++ src/components/SingleItem.js
@@ -3,10 +3,10 @@ import Select from './Select';
 import Moment from 'react-moment';
 import moment from 'moment/min/moment-with-locales';
 import 'moment-timezone';
+import EditItemForm from '../containers/EditItemForm';
 
 
-
-const SingleItem = ({item,backToItems,edit,auth,editNow,handleChange, categories, conditions, itemStatuses, destroyItem}) => {
+const SingleItem = ({item,backToItems,edit,auth,editNow, categories, conditions, itemStatuses, destroyItem}) => {
     return (
     <div className='eachItem'>
         <div className="back-to-list">
@@ -36,66 +36,18 @@ const SingleItem = ({item,backToItems,edit,auth,editNow,handleChange, categories
                 <b>Updated At: </b>{item ? <Moment date={item[0].updatedAt}/> : null}<br/>
             </div>
         </ol>
-            :  <div id="edit-item-form">
-                <form>
-                    <br />
-                    <div className="img-container-large">
-                        <img alt='Preview' className="fullsize" src={(item[0].image).slice(6)} />
-                    </div>
-                    <input type="file" accept="image/*" id="image-upload" placeholder="Upload New Image"/>
-                    <br />
-                    <br />
+            :   <EditItemForm
+                    name={item[0].name}
+                    image={item[0].image}
+                    description={item[0].description}
+                    price={item[0].price}
+                    manufacturer={item[0].manufacturer}
+                    model={item[0].model}
+                    dimensions={item[0].dimensions}
+                    notes={item[0].notes}
+                />
+            }
 
-                    Name:
-                    <br />
-                    <input type="text" placeholder={item[0].name} name="name" onChange={handleChange} />
-                    <br />
-                    <br />
-                    Status:
-                    <br />
-                    <Select name="status" handler={handleChange} list={itemStatuses} show="title"/>
-                    <br />
-                    <br />
-                    Description:
-                    <br />
-                    <textarea cols="50" rows="10" name="description" onChange={handleChange} defaultValue={item[0].description} />
-                    <br />
-                    <br />
-                    Price:
-                    <br />
-                    <input type="number" name="price" placeholder={item[0].price} onChange={handleChange} />
-                    <br />
-                    <br />
-                    Category:
-                    <br />
-                    <Select name="category" handler={handleChange} list={categories} show="title"/>
-                    <br />
-                    <br />
-                    Condition:
-                    <br />
-                    <Select name="condition" handler={handleChange} list={conditions} show="title"/>
-                    <br />
-                    <br />
-                    Manufacturer/Make:
-                    <br />
-                    <input type="text" name="manfucturer" placeholder={item[0].manufacturer} onChange={handleChange} />
-                    <br />
-                    <br />
-                    Model
-                    <br />
-                    <input type="text" name="model" placeholder={item[0].model} onChange={handleChange} />
-                    <br />
-                    <br />
-                    Dimensions:
-                    <br />
-                    <input type="text" name="dimensions" placeholder={item[0].dimensions} onChange={handleChange} />
-                    <br />
-                    <br />
-                    Notes:
-                    <br />
-                    <textarea cols="50" rows="10" name="notes" onChange={handleChange} defaultValue={item[0].notes} />
-                </form>
-             </div> }
             {auth ? <div className="delete-button"><button onClick={(e)=>destroyItem(item,e)}>
             Delete Item</button></div> : null}
             {auth ? <div className="submit-changes-button"><button onClick={(e)=>editNow(item,e)}>
