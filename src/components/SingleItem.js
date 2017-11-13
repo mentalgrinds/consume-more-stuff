378da390fe6d084477dd37be19cf99fc3bea76b2
Ella Mae A <ellamae207@gmail.com>
2017-11-13 13:48:47 -1000

    cleared unnecessary ols , replaced 1 to div

--- src/components/SingleItem.js
+++ src/components/SingleItem.js
@@ -13,7 +13,7 @@ const SingleItem = ({item,backToItems,edit,auth,editNow, categories, conditions,
             <button onClick={(e)=>backToItems(e)}>Back</button>
         </div>
     {!edit ?
-        <ol>
+        <div>
             <div className="detail-view-title">
                 <h2>{item ? item[0].name : null}</h2>
                 <div className="img-container-large">
@@ -35,7 +35,7 @@ const SingleItem = ({item,backToItems,edit,auth,editNow, categories, conditions,
                 <b>Posted At: </b>{item ? <Moment date={item[0].createdAt}/> : null}<br/>
                 <b>Updated At: </b>{item ? <Moment date={item[0].updatedAt}/> : null}<br/>
             </div>
-        </ol>
+        </div>
             :
                 <div className="blank-space-div">
                     <EditItemForm
--- src/components/UserDetailView.js
+++ src/components/UserDetailView.js
@@ -4,12 +4,10 @@ import React from 'react';
 const UserDetailView = ({id,username,password,email,userstatus,loadUser}) => {
   return (
     <div className='user-details'>
-      <ol>
         <b>User-Id:</b> {id}<br/>
         <span onClick={(e)=>loadUser(id,e)}><b>Username:</b>{username}</span><br/>
         <b>email:</b> {email}<br/>
         <b>User-Status</b> {userstatus}<br/>
-      </ol>
     </div>
   )
 }
