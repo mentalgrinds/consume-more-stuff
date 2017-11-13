584fc4043494e00ed724ba954317d49cc3b00484
Kaley Kriminger <kaley.kriminger@gmail.com>
2017-11-13 10:22:04 -1000

    updated styles

--- src/components/logo.js
+++ src/components/logo.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Logo = () => {
   return (
     <div id="logo">
-      <img alt='preview' id="logo-image" src={require('../kbe.svg')} />
+      <img alt='preview' id="logo-image" src="/logo.png" />
     </div>
   );
 }
