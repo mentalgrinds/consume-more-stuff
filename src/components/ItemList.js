5c6d7df275d45e1a928716f746fbd44255b66047
Ella Mae A <ellamae207@gmail.com>
2017-11-13 15:39:03 -1000

    cleared out comments

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -40,53 +40,7 @@ const ItemList = ({items,loadSingleItem,edit}) => {
 
   )
 }
-/*let newArr = _.sortBy(this.props.items, 'createdAt', function(n) {
-        return Math.sin(n)
-    });*/
 
-{/*const ItemList = ({items,loadSingleItem,edit}) => {
-  return (
-    <div className='allItemList'>
-      {
-        /*_.sortBy(items, 'createdAt', function(n) {
-            return Math.sin(n);
-          }
-        )
-
-        items.sort().map((item,idx) => {
-          return (
-
-
-
-            <ItemDetailView
-              loadSingleItem={loadSingleItem}
-              edit={edit}
-              key={idx}
-              id={item.id}
-              name={item.name}
-              description={item.description}
-              image={item.image}
-              price={item.price}
-              manufacturer={item.manufacturer}
-              model={item.model}
-              dimensions={item.dimensions}
-              notes={item.notes}
-              createdAt={item.createdAt}
-              updatedAt={item.updatedAt}
-              itemstatusId={item.itemstatus.title}
-              userId={item.seller.username}
-              conditionId={item.itemcondition.title}
-              categoryId={item.itemcategory.title}
-            />
-          )
-
-        })
-      }
-    </div>
-
-  )
-}
-*/}
 
 export default ItemList;
 
