e32102f4cc7604b938f43805a48bc613bb943d45
Jesse Copeland <jesse.copeland@gmail.com>
2017-11-17 10:41:14 -1000

    Add build files

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ const saltRounds      = 12;
 const PORT            = process.env.PORT || 8080;
 const app             = express();
 
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 //Authentication:
