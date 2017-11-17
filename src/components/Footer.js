0d06f0990c5506a7d50a60082b97a7bcc2e4ee82
Ella Mae A <ellamae207@gmail.com>
2017-11-16 14:15:10 -1000

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+const Footer = () => {
+  return (
+    <div className="footer">
+
+      <div className="footer_text"> [
+        hip,hip]    ©  2017 [ Kaley, Baseem & Ella ]
+      </div>
+      <div className="github">
+       <i class="fa fa-github fa-5x"></i>
+      </div>
+    </div>
+  )
+}
+
+
+
+
+
+
+
+export default Footer;
