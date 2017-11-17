235ed2d30a17708d93fe5901630ee543526b2b52
Ba5eem <baseem@gmail.com>
2017-11-17 09:12:51 -1000

    fixed msgs

--- src/lib/messageFilters/index.js
+++ src/lib/messageFilters/index.js
@@ -4,15 +4,15 @@ function filterItemMsg(arr,query){
     return elem.id === query;
   });
   let obj = data.pop();
-  local.image = (arr.length !== 0 ? obj.image : "http://via.placeholder.com/350x150")
-
-    Object.entries(local).forEach(([key, value]) => {
-      local.image = value;
-    //console.log(`${key} ${value}`); 
-    });
-    local.name = (arr.length !== 0 ? obj.name : null)
-    local.desc = (arr.length !== 0 ? obj.description : null)
-    local.price = (arr.length !== 0 ? obj.price : null)
+  local.image = "http://via.placeholder.com/350x150";
+local.image = "http://via.placeholder.com/350x150";
+    // Object.entries(local).forEach(([key, value]) => {
+    //   local.image = value;
+    // //console.log(`${key} ${value}`); 
+    // });
+    local.name = "name"
+    local.desc = "desc"
+    local.price = "price"
 
   return local;
 }
